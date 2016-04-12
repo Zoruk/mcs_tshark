54bdf03f5ec974ba8421c8215f3012bca501c8a5
Rick <rick.wertenbroek@heig-vd.ch>
2016-04-12 14:34:25 +0200

    Tests affichage final

--- test/tshark_tests.erl
+++ test/tshark_tests.erl
@@ -11,6 +11,18 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
+% Test du mode normal
+result_from_ping_pcap_test() ->
+    {ok, Expected} = file:read_file(data_dir("result_normal.txt")),
+    {ok, Actual} = tshark:tshark_from_file(data_dir("ping.pcap")),
+    ?assertEqual(binary_to_list(Expected), lists:flatten(Actual)).
+
+% Test du mode verbeux
+result_from_ping_pcap_verbose_test() ->
+    {ok, Expected} = file:read_file(data_dir("result_verbose.txt")),
+    {ok, Actual} = tshark:tshark_from_file_verbose(data_dir("ping.pcap")),
+    ?assertEqual(binary_to_list(Expected), lists:flatten(Actual)).
+
 simple_test() ->
   tshark:test("../ping.pcap"),
   ?assert(true).
